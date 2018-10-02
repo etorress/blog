19cd38e30b25a4dde23d2aefeafa9580b5be6350
etorress <eduardotbrs@gmail.com>
2018-10-02 14:09:06 -0700

    added a comment to the code

--- vendor/php/add_comment.php
+++ vendor/php/add_comment.php
@@ -5,6 +5,6 @@ $name = strip_tags(utf8_decode($_POST['name']));
 $comment = strip_tags(utf8_decode($_POST['comment']));
 
 $consulta = $conexion->query("INSERT INTO comments (name, comment, date_added) VALUES ('$name', '$comment', now())");
-
+// new comment
 //$insert = mysql_query("INSERT INTO comments (name, comment, date_added) VALUES ('$name', '$comment', now())", $conexion);
 ?>
